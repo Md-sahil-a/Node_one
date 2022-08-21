1470c397d2057f04a7e7bf041939755ce8ec1f64
md-sahil-a <mdsahilamaan2017@gmail.com>
2022-08-21 16:56:10 +0530

    update

--- kepler_spacecraft.js
+++ kepler_spacecraft.js
@@ -1,1 +1,15 @@
 const kepler = require('csv-parse');
+
+const fs  = require('fs');
+const result  = [];
+fs.createReadStream('DataOfKepler.csv')
+.on('data', (data)=>{
+    result.push(data);
+})
+.on('end', ()=>{
+    console.log(result);
+    console.log('Done');
+})
+.on('error', (error)=>{
+    console.log(error);
+})
